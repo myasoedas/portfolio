657b1fba9c64ab05be7c97a30426289c076b750f
myasoedas <myasoedas@yandex.ru>
2024-06-18 00:47:45 +0300

    В папке portfolio в файле models.py создать класс Project с атрибутами для описания проекта в портфолио.

--- personal_portfolio_project/portfolio/models.py
+++ personal_portfolio_project/portfolio/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    # Заголовок с названием проекта.
+    title = models.CharField(max_length=100)
+    # Краткое описание проекта.
+    description = models.CharField(max_length=250)
+    # Изображение главной страницы проекта.
+    image = models.ImageField(upload_to='portfolio/images/')
+    # Ссылка на проект в Интернете.
+    url = models.URLField(blank=True)
+
